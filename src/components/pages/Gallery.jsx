73b73b200eb1c13df58d6c33be3b9c19e1fe3cc3
shamim150 <161612149+shamim150@users.noreply.github.com>
2024-09-22 16:36:30 +0600

    gallery

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import Container from "../layer/Container";
+import TitleHeader from "../layer/TitleHeader";
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
-  )
-}
+    <div>
+      <Container>
+        <div>
+          <TitleHeader headerText="Show All " />
+          <div className="flex gap-10 items-center justify-center">
+            <img className="w-[400px]" src="/Gallery/Rice -Mill.jpg" alt="" />
+            <img className="w-[400px]" src="/Gallery/Rice -Mill.jpg" alt="" />
+            <img className="w-[400px]" src="/Gallery/Rice -Mill.jpg" alt="" />
+            <img className="w-[400px]" src="/Gallery/Rice -Mill.jpg" alt="" />
+          </div>
+        </div>
+        <div>
+          <TitleHeader headerText="Exhibition" />
+          <div className="flex items-center justify-center">
+            <img src="/Gallery/Rice -Mill.jpg" alt="" />
+          </div>
+        </div>
+        <div>
+          <TitleHeader headerText="Awards" />
+          <div className="flex items-center justify-center">
+            <img src="/Gallery/Rice -Mill.jpg" alt="" />
+          </div>
+        </div>
+        <div>
+          <TitleHeader headerText="Certificate" />
+          <div className="flex items-center justify-center">
+            <img src="/Gallery/Rice -Mill.jpg" alt="" />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-export default Gallery
+export default Gallery;
