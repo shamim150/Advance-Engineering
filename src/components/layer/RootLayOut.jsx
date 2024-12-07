3c06cb0631f693888561ca43efc93f4bb735903c
shamim150 <smshamimmahmud150@gmail.com>
2024-12-07 15:56:52 +0600

    z-work

--- src/components/layer/RootLayOut.jsx
+++ src/components/layer/RootLayOut.jsx
@@ -8,17 +8,21 @@ const RootLayOut = () => {
   return (
     <div className="overflow-hidden relative">
       <Link
-        className="fixed bottom-10 right-2 md:bottom-16 md:right-5 "
+        className="fixed bottom-10 right-2 md:bottom-16 md:right-5 z-50" // Add z-index here
         to="https://wa.me/+8801717507404?text=Hello"
         target="_blank"
       >
-        <img className="size-10 z-20 md:size-14" src="/whatsapp.gif" alt="whatsapp" />
+        <img
+          className="size-10 relative z-auto md:size-14"
+          src="/whatsapp.gif"
+          alt="whatsapp"
+        />
       </Link>
 
       <Navbar />
-    <div className="md:pt-10">
-    <Outlet />
-    </div>
+      <div className="md:pt-10">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
